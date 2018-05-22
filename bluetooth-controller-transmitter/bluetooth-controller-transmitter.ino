e0ca070a22d3f4b1329f7fa69d965c1f12576c71
karstvanderveen <38493863+karstvanderveen@users.noreply.github.com>
2018-05-22 15:38:43 +0200

    Controller
    knoppen en commentaar toegevoegd

--- bluetooth-controller-transmitter/bluetooth-controller-transmitter.ino
+++ bluetooth-controller-transmitter/bluetooth-controller-transmitter.ino
@@ -3,10 +3,10 @@
 #include <SoftwareSerial.h> //Serial library
 
 static const char *WAARDE[] = {
-    "n1.val=","n2.val=","n3.val=","n4.val="
+    "n0.val=","n1.val=","n2.val=","n3.val=","n4.val=","n8.val=","j0.val=","j1.val="
 };
 
-enum CONTROLS {
+enum CONTROLS {                           // Verzonden controls
   JSLX,
   JSLY,
   JSRX,
@@ -17,12 +17,12 @@ enum CONTROLS {
   BTN4,
 };
 
-enum CONFIG {
+enum CONFIG {                             // Verzonden modes
   MODE,
   SENSITIVITY
 };
 
-SoftwareSerial bt (5,6);  //RX, TX (Switched on the Bluetooth - RX -> TX | TX -> RX)
+SoftwareSerial bt (5,6);                  //RX, TX (Switched on the Bluetooth - RX -> TX | TX -> RX)
 
 int JoyStickLx;
 int JoyStickLy;
@@ -40,6 +40,16 @@ int btSensitivity = 255;
 int var1, var2, var3, var4;
 int Batterij, BatterijDisp;
 String Waarde;
+
+/*
+void waitForResponse() {
+    delay(1000);
+    while (mySerial.available()) {
+      Serial.write(mySerial.read());
+    }
+    Serial.write("\n");
+}*/
+
 
 // Buttons toevoegen---------------------
 NexNumber n5 = NexNumber(1, 16, "n5");
@@ -80,42 +90,28 @@ NexTouch *nex_listen_list[] =
     &page4,
     NULL };
 
-void einderegel() {
+void einderegel() {                                 // Einde bericht display
   Serial.write(0xff);
   Serial.write(0xff);
   Serial.write(0xff);}
 
-void h0PopCallback(void *ptr) {  
-  uint32_t slider = 0;
-  Serial.write("get h0.val");
-  delay(100);
-  recvRetNumber(&slider,1000);
-  Serial.println(slider);
-  }
-
-void b4PushCallback(void *ptr) {  
+void b4PushCallback(void *ptr) {                    // Uitvoer voor knop b4 van display (Deadzone instellen)
   Deadzone++;
-  einderegel();
-  Serial.print("n0.val=");
-  Serial.print(Deadzone);
-  einderegel();}
+  sendDisplay(WAARDE[0], Deadzone);}
 
-void b5PushCallback(void *ptr) {  
+void b5PushCallback(void *ptr) {                    // Uitvoer voor knop b5 van display (Deadzone instellen)
   if(Deadzone>0) {
   Deadzone--;
-  einderegel();
-  Serial.print("n0.val=");
-  Serial.print(Deadzone);}
-  einderegel();}
+  sendDisplay(WAARDE[0], Deadzone);}}
   
-void sendDisplay(String Waarde, int Beun) {
+void sendDisplay(String Waarde, int Beun) {         // Verstuurd analoge waardes naar display
   if(CurrentPage == 1) {
   einderegel();
   Serial.print(Waarde);
   Serial.print(Beun);
   einderegel();}}
 
-void sendControl(CONTROLS control, int state) {
+void sendControl(CONTROLS control, int state) {     // Verstuurd CONTROLS via bluetooth
   if(CurrentPage != 1) {
   bt.write("{0;");
   bt.print(control);
@@ -123,51 +119,43 @@ void sendControl(CONTROLS control, int state) {
   bt.print(state);
   bt.write("}");}}
 
-void sendConfig(CONFIG conf, int state) {
+void sendConfig(CONFIG conf, int state) {           // Verstuurd CONFIG via bluetooth
   bt.write("{1;");
   bt.print(conf);
   bt.write(":");
   bt.print(state);
   bt.write("}");}
 
-void page0PushCallback(void *ptr)  {
+void page0PushCallback(void *ptr)  {              // Init actie op pagina 0
   CurrentPage = 0;}
 
-void page1PushCallback(void *ptr)  {
-  einderegel();
-  Serial.print("j0.val=");
-  Serial.print(Sensitivity);
-  einderegel();
-  CurrentPage = 1;}
-
-void page2PushCallback(void *ptr)  {
+void page1PushCallback(void *ptr)  {              // Init actie op pagina 1
+  CurrentPage = 1;
+  sendDisplay(WAARDE[7], Sensitivity);
+  }
+    
+void page2PushCallback(void *ptr)  {              // Init actie op pagina 2
   CurrentPage = 2;}
 
-void page3PushCallback(void *ptr)  {
+void page3PushCallback(void *ptr)  {              // Init actie op pagina 3
   CurrentPage = 3;}
 
-void page4PushCallback(void *ptr)  {
+void page4PushCallback(void *ptr)  {              // Init actie op pagina 4
   CurrentPage = 4;}
 
-void b11PushCallback(void * ptr) {
+void b11PushCallback(void * ptr) {                // Actie bij push b11
   if(Sensitivity <= 90){
     Sensitivity = Sensitivity + 10;
     btSensitivity = btSensitivity + 13;
-    einderegel();
-    Serial.print("j1.val=");
-    Serial.print(Sensitivity);
-    einderegel();
+    sendDisplay(WAARDE[7], Sensitivity);
     sendConfig(SENSITIVITY, btSensitivity);}
 }
 
-void b12PushCallback(void * ptr) {
+void b12PushCallback(void * ptr) {                // Actie bij push b12
   if(Sensitivity >= 10){
     Sensitivity = Sensitivity - 10;
     btSensitivity = btSensitivity - 13;
-    einderegel();
-    Serial.print("j1.val=");
-    Serial.print(Sensitivity);
-    einderegel();
+    sendDisplay(WAARDE[7], Sensitivity);
     sendConfig(SENSITIVITY, btSensitivity);}
 }
 
@@ -178,6 +166,7 @@ void setup() {
   pinMode(11, INPUT);
   Serial.begin(9600);
   bt.begin(9600);
+// Displayfuncties aanmaken--------------------------------------------  
   b4.attachPush(b4PushCallback);
   b5.attachPush(b5PushCallback);
   b11.attachPush(b11PushCallback);
@@ -190,118 +179,128 @@ void setup() {
 };
 
 void loop() {
-nexLoop(nex_listen_list);
+nexLoop(nex_listen_list);                         // Luisteren of er data van display komt
+
+while (bt.available()) {
+  Serial.write(bt.read());
+  Serial.write("\n");
+}
 
 // ButtonControl-------------------------------------------------------
-if(digitalRead(8) != var1) {
+if(digitalRead(8) != var1) {                      // BTN0 wordt doorgestuurd via bt
     var1 = digitalRead(8);
     sendControl(BTN1, digitalRead(8));}
 
-if(digitalRead(9) != var2) {
+if(digitalRead(9) != var2) {                      // BTN1 wordt doorgestuurd via bt
     var2 = digitalRead(9);
     sendControl(BTN2, digitalRead(9));}
+
+if(digitalRead(10) != var3) {                     // BTN2 wordt doorgestuurd via bt
+    var3 = digitalRead(10);
+    sendControl(BTN3, digitalRead(10));}
+
+if(digitalRead(11) != var4) {                     // BTN3 wordt doorgestuurd via bt
+    var4 = digitalRead(11);
+    sendControl(BTN4, digitalRead(11));}
   
 // BatterijSpanning------------------------------------------------------
 // 150 tot 464 (analog - 512)
 // -4 tot 310 (analog - 666) 6.6V - 9.6V
 
-if(abs((analogRead(A4)-662) - Batterij) > 3) {
+if(abs((analogRead(A4)-666) - Batterij) > 20) {   // Batterijspanning inlezen
   Batterij = (analogRead(A4)-666);
   BatterijDisp = Batterij * 100 / 310;
-  einderegel();
-  Serial.print("n8.val=");
-  Serial.print(BatterijDisp);
   einderegel();
   Serial.print("j0.val=");
   Serial.print(BatterijDisp);
   einderegel();}
   
 // JoyStickLeftx---------------------------------------------------------
-JoyStickLx = analogRead(A0)-511;                     // Joystick uitlezen
+JoyStickLx = analogRead(A0)-511;                     // Joystick JSLX uitlezen
 if(JoyStickLx < Deadzone and JoyStickLx > -Deadzone) {
   if(x1 != 0) {
     x1 = 0;
     sendControl(JSLX, x1);
-    sendDisplay(WAARDE[0], x1);}}
+    sendDisplay(WAARDE[1], x1);}}
 else if(JoyStickLx > (511 - Deadzone)) {
   if(x1 != 511) {
     x1 = 511;
     sendControl(JSLX, x1);
-    sendDisplay(WAARDE[0], x1);}}
+    sendDisplay(WAARDE[1], x1);}}
 else if(JoyStickLx < (Deadzone - 511)) {
   if(x1 != -511) {
     x1 = -511;
     sendControl(JSLX, x1);
-    sendDisplay(WAARDE[0], x1);}}
+    sendDisplay(WAARDE[1], x1);}}
 else if(abs(JoyStickLx - x1) > 2) {
   x1 = JoyStickLx;
   sendControl(JSLX, JoyStickLx);
-  sendDisplay(WAARDE[0], JoyStickLx);}
+  sendDisplay(WAARDE[1], JoyStickLx);}
  
 // JoyStickLefty---------------------------------------------------------
-JoyStickLy = analogRead(A1)-511;                     // Joystick uitlezen
+JoyStickLy = analogRead(A1)-511;                     // Joystick JSLY uitlezen
 if(JoyStickLy < Deadzone and JoyStickLy > -Deadzone) {
   if(y1 != 0) {
     y1 = 0;
     sendControl(JSLY, y1);
-    sendDisplay(WAARDE[1], y1);}}
+    sendDisplay(WAARDE[2], y1);}}
 else if(JoyStickLy > (511 - Deadzone)) {
   if(y1 != 511) {
     y1 = 511;
     sendControl(JSLY, y1);
-    sendDisplay(WAARDE[1], y1);}}
+    sendDisplay(WAARDE[2], y1);}}
 else if(JoyStickLy < (Deadzone - 511)) {
   if(y1 != -511) {
     y1 = -511;
     sendControl(JSLY, y1);
-    sendDisplay(WAARDE[1], y1);}}
+    sendDisplay(WAARDE[2], y1);}}
 else if(abs(JoyStickLy - y1) > 2) {
   y1 = JoyStickLy;
   sendControl(JSLY, JoyStickLy);
-  sendDisplay(WAARDE[1], JoyStickLy);}
+  sendDisplay(WAARDE[2], JoyStickLy);}
   
 // JoyStickRightx---------------------------------------------------------
-JoyStickRx = analogRead(A2)-511;                      // Joystick uitlezen
+JoyStickRx = analogRead(A2)-511;                      // Joystick JSRX uitlezen
 if(JoyStickRx < Deadzone and JoyStickRx > -Deadzone) {
   if(x2 != 0) {
     x2 = 0;
     sendControl(JSRX, x2);
-    sendDisplay(WAARDE[2], x2);}}
+    sendDisplay(WAARDE[3], x2);}}
 else if(JoyStickRx > (511 - Deadzone)) {
   if(x2 != 511){
     x2 = 511;
     sendControl(JSRX, x2);
-    sendDisplay(WAARDE[2], x2);}}
+    sendDisplay(WAARDE[3], x2);}}
 else if(JoyStickRx < (Deadzone - 511)) {
   if(x2 != -511) {
     x2 = -511;
     sendControl(JSRX, x2);
-    sendDisplay(WAARDE[2], x2);}}
+    sendDisplay(WAARDE[3], x2);}}
 else if(abs(JoyStickRx - x2) > 2) {
   x2 = JoyStickRx;
   sendControl(JSRX, JoyStickRx);
-  sendDisplay(WAARDE[2], x2);}
+  sendDisplay(WAARDE[3], x2);}
 
 // JoyStickRighty---------------------------------------------------------
-JoyStickRy = analogRead(A3)-511;                      // Joystick uitlezen
+JoyStickRy = analogRead(A3)-511;                      // Joystick JSRY uitlezen
 if(JoyStickRy < Deadzone and JoyStickRy > -Deadzone) {
   if(y2 != 0){
     y2  =0;
     sendControl(JSRY, y2);
-    sendDisplay(WAARDE[3], y2);}}
+    sendDisplay(WAARDE[4], y2);}}
 else if(JoyStickRy > (511 - Deadzone)) {
   if(y2!=511){
     y2=511;
     sendControl(JSRY, y2);
-    sendDisplay(WAARDE[3], y2);}}
+    sendDisplay(WAARDE[4], y2);}}
 else if(JoyStickRy < (Deadzone - 511)) {
   if(y2 != -511) {
     y2 = -511;
     sendControl(JSRY, y2);
-    sendDisplay(WAARDE[3], y2);}}
+    sendDisplay(WAARDE[4], y2);}}
 else if(abs(JoyStickRy - y2) > 2) {
   y2 = JoyStickRy;
   sendControl(JSRY, JoyStickRy);
-  sendDisplay(WAARDE[3], y2);}  
+  sendDisplay(WAARDE[4], y2);}  
 delay(10);
 };
